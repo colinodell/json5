4d0ba725cff253c8bcc57979db177c9485b2fe78
Colin O'Dell <colinodell@gmail.com>
2017-11-05 19:53:11 -0500

    Implement JSON5 tests

--- test/Functional/ParseTest.php
+++ test/Functional/ParseTest.php
@@ -3,6 +3,7 @@
 namespace ColinODell\Json5\Test\Functional;
 
 use ColinODell\Json5\Json5Decoder;
+use ColinODell\Json5\SyntaxError;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Finder\Finder;
 
@@ -33,5 +34,124 @@ class ParseTest extends TestCase
         }
 
         return $tests;
+    }
+
+    /**
+     * @param string      $json
+     * @param string      $expected
+     * @param string|null $expectedAssoc
+     *
+     * @dataProvider dataForTestJson5Parsing
+     */
+    public function testJson5Parsing($json, $expected, $expectedAssoc = null)
+    {
+        $this->assertSame($expected, var_export(Json5Decoder::decode($json), true));
+
+        if ($expectedAssoc !== null) {
+            $this->assertSame($expectedAssoc, var_export(Json5Decoder::decode($json, true), true));
+        }
+    }
+
+    /**
+     * @return array
+     */
+    public function dataForTestJson5Parsing()
+    {
+        $finder = new Finder();
+        $finder->files()->in(__DIR__.'/data/*')->name('*.json5');
+
+        $tests = array();
+        foreach ($finder as $file) {
+            $data = explode('////////// EXPECTED OUTPUT: //////////', file_get_contents($file));
+            $tests[] = array(
+                $data[0],
+                trim($data[1]),
+                isset($data[2]) ? trim($data[2]) : null,
+            );
+        }
+
+        return $tests;
+    }
+
+    /**
+     * @param string $json
+     *
+     * @dataProvider dataForTestValidES5DisallowedByJson5
+     */
+    public function testValidES5DisallowedByJson5($json)
+    {
+        $this->setExpectedException('ColinODell\\Json5\\SyntaxError');
+        Json5Decoder::decode($json);
+    }
+
+    /**
+     * @return array
+     */
+    public function dataForTestValidES5DisallowedByJson5()
+    {
+        $finder = new Finder();
+        $finder->files()->in(__DIR__.'/data/*')->name('*.js');
+
+        $tests = array();
+        foreach ($finder as $file) {
+            $tests[] = array(file_get_contents($file));
+        }
+
+        return $tests;
+    }
+
+    /**
+     * @param string $json
+     * @param array  $expectedError
+     *
+     * @dataProvider dataForTestInvalidES5WhichIsAlsoInvalidJson5
+     */
+    public function testInvalidES5WhichIsAlsoInvalidJson5($json, $expectedError)
+    {
+        try {
+            Json5Decoder::decode($json);
+            $this->fail('Invalid ES5/JSON5 should fail');
+        } catch (SyntaxError $e) {
+            if ($expectedError !== null) {
+                $this->assertEquals($expectedError['lineNumber'], $e->getLineNumber());
+                $this->assertEquals($expectedError['columnNumber'], $e->getColumn());
+                $this->assertStringStartsWith($expectedError['message'], $e->getMessage());
+            }
+
+            return $this->assertTrue(true);
+        }
+
+        $this->fail('Invalid ES5/JSON5 should fail');
+    }
+
+    /**
+     * @return array
+     */
+    public function dataForTestInvalidES5WhichIsAlsoInvalidJson5()
+    {
+        $finder = new Finder();
+        $finder->files()->in(__DIR__.'/data/*')->name('*.txt');
+
+        $tests = array();
+        foreach ($finder as $file) {
+            $tests[] = array(
+                file_get_contents($file),
+                $this->getErrorSpec($file),
+            );
+        }
+
+        return [$tests[24]];
+    }
+
+    private function getErrorSpec($file)
+    {
+        $errorSpec = str_replace('.txt', '.errorSpec', $file);
+        if (!file_exists($errorSpec)) {
+            return null;
+        }
+
+        $spec = json_decode(file_get_contents($errorSpec), true);
+
+        return $spec;
     }
 }
